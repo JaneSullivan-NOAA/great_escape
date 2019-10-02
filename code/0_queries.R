362152448d4623e7bbf35b041952a5ebbf584643
unknown <jysullivan@DFGJNUDCF119359.dfg.alaska.local>
2019-10-02 12:21:28 -0800

    add specimen number to bio data, query effort data

--- code/0_queries.R
+++ code/0_queries.R
@@ -5,10 +5,10 @@
 
 # All final columns formatted as follows unless otherwise specified:
 # length: fork length, cm
+# girth: mm
 # weight: kg
-# depth: average depth, meters
+# depth: start, end, average depth - meters
 # date: ISO 8601 YYYY/MM/DD
-# characters & factors - first letter capitilized (e.g. 'Sex'), otherwise lowercase
 # lat and lon are in decimal degrees
 
 YEAR <- 2019 # study year(s)
@@ -34,12 +34,12 @@ zprod_channel <- dbConnect(drv = dbDriver('Oracle'),
                            password = ora$zprod_pw, 
                            dbname = zprod)
 
-# Queries -----
+# Pot bio data -----
 
-# Experimental code for escape ring study = 66 
+# Experimental project code for escape ring study = 66 
 query <- 
   paste0(
-" select  year, effort_no, 
+" select  year, effort_no, specimen_no,
           length_millimeters, weight_kilograms, girth_millimeters,
           pot_treatment_code, pot_treatment
                    
@@ -54,12 +54,44 @@ dbGetQuery(zprod_channel, query) -> pot_bio
 write_csv(pot_bio, paste0("data/raw/pot_bio_", YEAR, ".csv"))
 
 read_csv(paste0("data/raw/pot_bio_", YEAR, ".csv"), guess_max = 50000) %>% 
-  mutate(Treatment = derivedFactor("Control" = POT_TREATMENT_CODE == "99",
+  mutate(treatment = derivedFactor("Control" = POT_TREATMENT_CODE == "99",
                                    "4.00 in" = POT_TREATMENT_CODE == "00",
                                    "3.75 in" = POT_TREATMENT_CODE == "01",
                                    "3.50 in" = POT_TREATMENT_CODE == "02",
                                    .default = NA),
-         length_cm = LENGTH_MILLIMETERS / 10) %>% 
-  select(year = YEAR, effort_no = EFFORT_NO, Treatment, length_cm, weight_kg = WEIGHT_KILOGRAMS,
-         girth_mm = GIRTH_MILLIMETERS) %>% 
+         length = LENGTH_MILLIMETERS / 10) %>% 
+  select(year = YEAR, effort_no = EFFORT_NO, specimen_no = SPECIMEN_NO, 
+         treatment, length, weight = WEIGHT_KILOGRAMS,
+         girth = GIRTH_MILLIMETERS) %>% 
   write_csv(paste0("data/pot_bio_", YEAR, ".csv"))
+
+# Pot effort data ----
+
+# Note this uses project code = 11 (the normal code for the Chatham pot survey)
+query <-
+  paste0(
+" select *
+
+  from out_g_sur_pot
+
+  where species_code = '710' and
+        year = ", YEAR)
+
+dbGetQuery(zprod_channel, query) -> pot_effort
+
+write_csv(pot_effort, paste0("data/raw/pot_effort_", YEAR, ".csv"))
+
+read_csv(paste0("data/raw/pot_effort_", YEAR, ".csv"), guess_max = 50000) %>%
+  mutate(
+    # Define soak time NEEDS_REVIEW
+    soak_time = difftime(TIME_FIRST_ANCHOR_ONBOARD, TIME_SECOND_ANCHOR_OVERBOARD, units = "hours"),
+    start_depth = START_DEPTH_FATHOMS * 1.8288, # fathoms to meters
+    end_depth = END_DEPTH_FATHOMS * 1.8288,
+    mean_depth = AVG_DEPTH_FATHOMS * 1.8288) %>% 
+  select(year = YEAR, effort_no = EFFORT_NO, stat = G_STAT_AREA,
+         start_lat = START_LATITUDE_DECIMAL_DEGREES, start_lon = START_LONGITUDE_DECIMAL_DEGREE,
+         end_lat = END_LATITUDE_DECIMAL_DEGREES, end_lon = END_LONGITUDE_DECIMAL_DEGREES,
+         soak_time, start_depth, end_depth, mean_depth, n_pots = NUMBER_OF_POTS_RETRIEVED, 
+         discard_status_code = DISCARD_STATUS_CODE, discard_status = DISCARD_STATUS,
+         n = NUMBERS, comments = POT_COMMENTS) %>% 
+  write_csv(paste0("data/pot_effort_", YEAR, ".csv"))
