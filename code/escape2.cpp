e558d0383e1e386290f8c92ceebd81d24c928ffb
Jane Sullivan <jane.sullivan1@alaska.gov>
2019-11-25 13:47:18 -0900

    Get rid of linear regression between treatment parameters. Didn't help model fitting.

--- code/escape2.cpp
+++ code/escape2.cpp
@@ -45,23 +45,11 @@ Type objective_function<Type>::operator() ()
   PARAMETER_VECTOR(s50);
   PARAMETER_VECTOR(slp);     
   
-  // relative probability of entering a control pot (account for possible
+  // Relative probability of entering a control pot (account for possible
   // difference in the degree to which fish are attracted to escape-ring and to
   // control trap). This one parameter is shared between treatments.
   PARAMETER(log_delta);
   Type delta = exp(log_delta);  
-  
-  // Linear regression coefficients governing the relationship between length at
-  // 50% selectivity (s50) and escape ring diameter (trt): s50 ~ trt
-  PARAMETER(a1);
-  PARAMETER(log_b1);
-  Type b1 = exp(log_b1);  // estimate in log space to assume positive slope? not sure this is necessary
-
-  // // Linear regression coefficients governing the relationship between the slope
-  // // of the logistic selectivity curve (slx_slp) and escape ring diameter (trt):
-  // // slx_slp ~ trt
-  // PARAMETER(a2);
-  // PARAMETER(b2);  
   
   // Random effects for set [j], shared between treatments.
   PARAMETER_VECTOR(nu);
@@ -115,8 +103,6 @@ Type objective_function<Type>::operator() ()
 
   // FITTED VALUES
 
-
-  
   // Fitted selectivity  
   matrix<Type> fit_slx(nlen,ntrt);
   fit_slx.setZero();
@@ -127,6 +113,7 @@ Type objective_function<Type>::operator() ()
     }
   }
   
+  // Fit selectivity across a wider range of lengths for mcmc
   int nfit = fit_len.size();
   matrix<Type> full_slx(nfit,ntrt);
   full_slx.setZero();
@@ -146,41 +133,6 @@ Type objective_function<Type>::operator() ()
     fit_phi(i,k) = fit_slx(i,k) / (fit_slx(i,k) + delta);
     }
   }
-
-  // Length at 25%, 50%, and 75%
-  // vector<Type> s25(ntrt);
-  // vector<Type> s50(ntrt);
-  // vector<Type> s75(ntrt);
-  // 
-  // for (int k = 0; k < ntrt; k++) {
-  //   s25(k) = - (alpha(k) + log(Type(3))) / (beta(k));
-  //   s50(k) = - alpha(k) / beta(k);
-  //   s75(k) = - (alpha(k) - log(Type(3))) / (beta(k));
-  // }
-
-  // Selection range (slx_rng, difference between s75 and s25); selection factor (slx_fct,
-  // s50 weighted by escape ring size, trt); slope of the logistic curve
-  // (slx_slp)
-  // vector<Type> slx_rng(ntrt);
-  // vector<Type> slx_fct(ntrt);
-  // vector<Type> slx_slp(ntrt);
-  // 
-  // for (int k = 0; k < ntrt; k++) {
-  //   slx_rng(k) = s75(k) - s50(k);
-  //   slx_fct(k) = s50(k) * trt(k);
-  //   slx_slp(k) = beta(k) / Type(4);
-  // }
-
-  // Linear regression between length at 50% selectivity and escape ring diameter
-  for (int k = 0; k < ntrt; k++) {
-    s50(k) = a1 + b1 * trt(k);
-  }
-
-  // // Linear regression between slope of logistic selectivity curve and escape
-  // // ring diameter
-  // for (int k = 0; k < ntrt; k++) {
-  //   slp(k) = a2 + b2 * trt(k);
-  // }
 
   // OBJECTIVE FUNCTION -----
 
@@ -203,25 +155,6 @@ Type objective_function<Type>::operator() ()
     set_effect -= dnorm(nu(j), Type(0), Type(1), true);
   }
   nll += set_effect;
-
-  // What might the penalized likelihood be for a linear regression? One option
-  // is to use the parameter estimates as the observed and the estimates from
-  // the theoretical selectivity curves as the predicted, and use sum of squares
-  // as the nll
-
-  Type penl_s50 = 0;
-  Type penl_slp = 0;
-
-  for (int k = 0; k < ntrt; k++) {
-    penl_s50 += square(theor_s50(k) - s50(k));
-    // penl_slp += square(theor_slp(k) - slp(k));
-  }
-
-  penl_s50 *= wt_s50;  // weight for the s50 penalty
-  // penl_slp *= wt_slp;  // weight for the slp penalty
-  // 
-  nll += penl_s50;
-  // nll += penl_slp;
 
   // Priors to constrain selectivity curve at 0 and 100
   Type s0 = 0;
@@ -246,23 +179,13 @@ Type objective_function<Type>::operator() ()
   // REPORT SECTION -----
   
   REPORT(fit_slx);
-  // REPORT(alpha);
-  // REPORT(beta);
   REPORT(s50);
   REPORT(slp);
   REPORT(slx);
   REPORT(phi);
   REPORT(fit_phi);
   REPORT(r);
-  // REPORT(s50);
-  // REPORT(s25);
-  // REPORT(s75);
-  // REPORT(slx_rng);
-  // REPORT(slx_fct);
-  // REPORT(slx_slp);
   REPORT(set_effect);
-  // REPORT(penl_s50);
-  // REPORT(penl_slp);
   REPORT(nll);
   REPORT(prior_s0);
   REPORT(prior_s100);
